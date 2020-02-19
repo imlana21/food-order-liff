6959156f83cf9179e638f13d6dbc7009cfe81382
lana404 <lliet.404@gmail.com>
2020-02-19 11:38:13 +0700

    First Commit

---
+++ js/config.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+	// Event Listener Button Login
+	$('#liffLoginButton').on('click', function() {
+		//Cek Apakah user sudah login
+		if (!liff.isLoggedIn()) {
+			//Jika belum maka akan diarahkan ke Line Login
+			liff.login();
+		}
+	});
+	// Event Listener Button Login
+	$('#liffLogoutButton').on('click', function() {
+		if (liff.isLoggedIn() ) {
+			liff.logout();
+			window.location.reload();
+		}
+	});
+
+	if (liff.isLoggedIn()) {
+		
+	} else {
+		alert('Logout');
+	}
+})
